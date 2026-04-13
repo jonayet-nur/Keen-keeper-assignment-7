0b6cbc31245f8bb8ef7a25f852b0d8e3d22ac1e8
jonayet-nur <jonayetnur@gmail.com>
2026-04-13 21:36:24 +0600

    File structure and enviroment setup

---
+++ src/Components/Homepagebanner/AllfriendCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AllfriendCard = () => {
+  return (
+    <div>AllfriendCard</div>
+  )
+}
+
+export default AllfriendCard
---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+// import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    {/* <h2 className='text-2xl bg-fuchsia-600'>Hello World</h2>
+    <button className='btn btn-primary'>Click me</button> */}
+  </StrictMode>,
+)
