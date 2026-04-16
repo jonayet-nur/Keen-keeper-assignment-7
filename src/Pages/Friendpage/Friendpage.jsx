90694529eb533e478215e7d1b19b539221d3ca2f
jonayet-nur <jonayetnur@gmail.com>
2026-04-17 00:57:33 +0600

    timeline filter added

--- src/Pages/Friendpage/Friendpage.jsx
+++ src/Pages/Friendpage/Friendpage.jsx
@@ -1,10 +1,16 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { FriendContext } from '../../context/TimelineContext'
 import TimelineCard from '../../ui/TimelineCard'
 
 const Friendpage = () => {
 
   const {storedTimeline} = useContext(FriendContext)
+  const [filter, setFilter] = useState("all")
+
+const filteredTimeline =
+filter === "all"
+? storedTimeline
+: storedTimeline.filter(item => item.type === filter)
   return (
     // <div className='container mx-auto'>
     //     <h1>FriendPage</h1>
@@ -15,8 +21,20 @@ const Friendpage = () => {
     <div className='bg-[#F8FAFC]'>
             <div className='container mx-auto py-10 '>
                 <h2 className='text-[48px] font-bold'>Timeline </h2>
+               {/* Filter Dropdown */}
+<div className="my-4">
+<select
+className="border p-4 rounded-lg"
+onChange={(e)=> setFilter(e.target.value)}
+>
+<option value="all">Filter timeline</option>
+<option value="call">Call</option>
+<option value="text">Text</option>
+<option value="video">Video Call</option>
+</select>
+</div>
                 {
-                    storedTimeline.map(data => <TimelineCard data={data}></TimelineCard>)
+                    filteredTimeline.map(data => <TimelineCard data={data}></TimelineCard>)
                 }
             </div>
         </div>
